1d79225fc34684ccd7551541c55ed4465ecf3f65
Ninh Nguyen <nguyentienhaininh@gmail.com>
2021-08-08 03:00:29 +0700

    update validate new user form

--- src/app/main/apps/identity-provider/users/user/user-list/new-user-sidebar/new-user-sidebar.component.ts
+++ src/app/main/apps/identity-provider/users/user/user-list/new-user-sidebar/new-user-sidebar.component.ts
@@ -1,49 +1,69 @@
 import { Component, OnInit } from '@angular/core';
-import { CoreSidebarService } from '@core/components/core-sidebar/core-sidebar.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-new-user-sidebar',
   templateUrl: './new-user-sidebar.component.html'
 })
 export class NewUserSidebarComponent implements OnInit {
-  public username;
-  public email;
-  public password;
-  public firstName;
-  public lastName;
-  public phoneNo;
-  public role = [ 'ADMIN', 'SUPERADMIN', 'OPERATOR',   
-
-    'USER-PERSONAL', 'USER-ORGANIZATION', 'USER-SERVICE', 'USER-DEVICE' ];
-  public createdAt = new Date();
+  public ReactiveUserDetailsForm: FormGroup;
+  public ReactiveUDFormSubmitted = false;
+  public UDForm = {
+    username: '',
+    email:'',
+    password:'',
+    firstName:'',
+    lastName:'',
+    phoneNo:'',
+    role:'',
+    createdAt : new Date(),
+    confPassword:'',
+  };
+  roles = [ 'ADMIN', 'SUPERADMIN', 'OPERATOR',   
+  
+  'USER-PERSONAL', 'USER-ORGANIZATION', 'USER-SERVICE', 'USER-DEVICE' ]
 
   /**
    *' Constructor
    *
-   * @param {CoreSidebarService} _coreSidebarService
+   * @param {NgbModal} modalService
    */
-  constructor(private _coreSidebarService: CoreSidebarService) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private modalService: NgbModal
+    ) {}
 
-  /**
-   * Toggle the sidebar
-   *
-   * @param name
-   */
-  toggleSidebar(name): void {
-    this._coreSidebarService.getSidebarRegistry(name).toggleOpen();
+  get ReactiveUDForm() {
+    return this.ReactiveUserDetailsForm.controls;
   }
 
-  /**
-   * Submit
-   *
-   * @param form
-   */
-  submit(form) {
-    if (form.valid) {
-      this.toggleSidebar('new-user-sidebar');
+  ReactiveUDFormOnSubmit() {
+    this.ReactiveUDFormSubmitted = true;
+
+    // stop here if form is invalid
+    if (this.ReactiveUserDetailsForm.invalid) {
+      return;
     }
+
+    alert('Đăng kí thành công');
   }
 
-  ngOnInit(): void {}
+  toggleSidebar(){
+    this.modalService.dismissAll();
+  }
+  
+  ngOnInit(): void {
+    this.ReactiveUserDetailsForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confPassword: ['', [Validators.required, Validators.minLength(6)]],
+      phoneNo: ['', [Validators.required]],
+    });
+  }
 }
 
