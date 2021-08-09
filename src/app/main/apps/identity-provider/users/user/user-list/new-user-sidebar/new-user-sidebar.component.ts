08a5c2e282e2aab0fcb3b6dab87fc181faa056d4
Ninh Nguyen <nguyentienhaininh@gmail.com>
2021-08-10 03:01:43 +0700

    update new user form

--- src/app/main/apps/identity-provider/users/user/user-list/new-user-sidebar/new-user-sidebar.component.ts
+++ src/app/main/apps/identity-provider/users/user/user-list/new-user-sidebar/new-user-sidebar.component.ts
@@ -10,6 +10,9 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class NewUserSidebarComponent implements OnInit {
   public ReactiveUserDetailsForm: FormGroup;
   public ReactiveUDFormSubmitted = false;
+  public roles = [ 'ADMIN', 'SUPERADMIN', 'OPERATOR',   
+  
+  'USER-PERSONAL', 'USER-ORGANIZATION', 'USER-SERVICE', 'USER-DEVICE' ];
   public UDForm = {
     username: '',
     email:'',
@@ -21,9 +24,6 @@ export class NewUserSidebarComponent implements OnInit {
     createdAt : new Date(),
     confPassword:'',
   };
-  roles = [ 'ADMIN', 'SUPERADMIN', 'OPERATOR',   
-  
-  'USER-PERSONAL', 'USER-ORGANIZATION', 'USER-SERVICE', 'USER-DEVICE' ]
 
   /**
    *' Constructor
@@ -39,6 +39,10 @@ export class NewUserSidebarComponent implements OnInit {
     return this.ReactiveUserDetailsForm.controls;
   }
 
+  toggleSidebar(){
+    this.modalService.dismissAll();
+  }
+
   ReactiveUDFormOnSubmit() {
     this.ReactiveUDFormSubmitted = true;
 
@@ -47,15 +51,13 @@ export class NewUserSidebarComponent implements OnInit {
       return;
     }
 
-    alert('Đăng kí thành công');
+    alert('Đăng kí thành công\nThông tin đăng kí:Tên tài khoản: ' + this.UDForm.username + '\nHọ và tên: ' + this.UDForm.lastName + this.UDForm.firstName + '\nSố điện thoại: ' + this.UDForm.phoneNo + '\nEmail: ' + this.UDForm.email + '\nPassword: ' + this.UDForm.password + '\nVai trò: ' + this.UDForm.role + '\nNgày đăng kí: ' + this.UDForm.createdAt);
+    this.toggleSidebar(); 
   }
 
-  toggleSidebar(){
-    this.modalService.dismissAll();
-  }
-  
   ngOnInit(): void {
-    this.ReactiveUserDetailsForm = this.formBuilder.group({
+    this.ReactiveUserDetailsForm = this.formBuilder.group(
+    {
       username: ['', Validators.required],
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
@@ -63,7 +65,29 @@ export class NewUserSidebarComponent implements OnInit {
       password: ['', [Validators.required, Validators.minLength(6)]],
       confPassword: ['', [Validators.required, Validators.minLength(6)]],
       phoneNo: ['', [Validators.required]],
-    });
+    },
+    {
+        validator: MustMatch('password', 'confPassword')
+    }
+    );
   }
 }
+  export function MustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+  
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+        // return if another validator has already found an error on the matchingControl
+        return;
+      }
+  
+      // set error on matchingControl if validation fails
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
+  }
 
