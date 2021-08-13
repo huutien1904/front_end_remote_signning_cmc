7f96b65f5500f8ad2a7fd9481b301776328d7137
Ninh Nguyen <nguyentienhaininh@gmail.com>
2021-08-13 10:13:08 +0700

    update api new user form

--- src/app/main/apps/identity-provider/users/user/user-list/new-user-sidebar/new-user-sidebar.component.ts
+++ src/app/main/apps/identity-provider/users/user/user-list/new-user-sidebar/new-user-sidebar.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'environments/environment';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-user-sidebar',
@@ -11,29 +13,29 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class NewUserSidebarComponent implements OnInit {
   public ReactiveUserDetailsForm: FormGroup;
   public ReactiveUDFormSubmitted = false;
+  public confPassword = '';
   public roles = [ 'ADMIN', 'SUPERADMIN', 'OPERATOR',   
-  
   'USER-PERSONAL', 'USER-ORGANIZATION', 'USER-SERVICE', 'USER-DEVICE' ];
   public UDForm = {
     username: '',
-    email:'',
-    password:'',
-    firstName:'',
-    lastName:'',
-    phoneNo:'',
-    role:'',
-    createdAt : new Date(),
-    confPassword:'',
+    password: '',
+    firstName: '',
+    lastName: '',
+    phoneNo: '',
+    email: '',
+    role: '',
   };
 
   /**
    *' Constructor
    *
    * @param {NgbModal} modalService
+   * @param {HttpClient} _httpClient
    */
   constructor(
     private formBuilder: FormBuilder,
-    private modalService: NgbModal
+    private modalService: NgbModal,
+    private _httpClient: HttpClient
     ) {}
 
   get ReactiveUDForm() {
@@ -44,16 +46,25 @@ export class NewUserSidebarComponent implements OnInit {
     this.modalService.dismissAll();
   }
 
-  ReactiveUDFormOnSubmit() {
+  ReactiveUDFormOnSubmit() { 
     this.ReactiveUDFormSubmitted = true;
-
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    const token = currentUser.token;
+    const bodyRequest = this.UDForm;
+    const option = {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + token,
+        "Access-Control-Allow-Origin": "*",
+      }
+    };
     // stop here if form is invalid
     if (this.ReactiveUserDetailsForm.invalid) {
       return;
     }
-
-    alert('Đăng kí thành công\nThông tin đăng kí:Tên tài khoản: ' + this.UDForm.username + '\nHọ và tên: ' + this.UDForm.lastName + this.UDForm.firstName + '\nSố điện thoại: ' + this.UDForm.phoneNo + '\nEmail: ' + this.UDForm.email + '\nPassword: ' + this.UDForm.password + '\nVai trò: ' + this.UDForm.role + '\nNgày đăng kí: ' + this.UDForm.createdAt);
-    this.toggleSidebar(); 
+    return this._httpClient.post<any>(`${environment.apiUrl}/user/register`, bodyRequest, option).subscribe((response: any) => {
+      console.log(response);
+    })
   }
 
   ngOnInit(): void {
@@ -63,8 +74,8 @@ export class NewUserSidebarComponent implements OnInit {
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      confPassword: ['', [Validators.required, Validators.minLength(6)]],
+      password: ['', [Validators.required, Validators.minLength(1)]],
+      confPassword: ['', [Validators.required, Validators.minLength(1)]],
       phoneNo: ['', [Validators.required]],
     },
     {
