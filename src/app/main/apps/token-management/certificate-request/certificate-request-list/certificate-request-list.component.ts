345ede7fb63ab37f856bd484aebce7e67fc40075
Ninh Nguyen <nguyentienhaininh@gmail.com>
2021-08-23 22:58:40 +0700

    add yeu cau chung thuc

--- src/app/main/apps/token-management/certificate-request/certificate-request-list/certificate-request-list.component.ts
+++ src/app/main/apps/token-management/certificate-request/certificate-request-list/certificate-request-list.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-certificate-request-list',
   templateUrl: './certificate-request-list.component.html',
-  styleUrls: ['./certificate-request-list.component.scss']
+  styleUrls: ['./certificate-request-list.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CertificateRequestListComponent implements OnInit {
-
+  public moreOption = true;
+  public tested = [];
+  public test: any = [{name: '1'}, {name: '2'}, {name: '3'}];
   constructor() { }
 
   ngOnInit(): void {
