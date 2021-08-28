eef010cb59c19b344cab5cb0d73849f992ce0c01
Ninh Nguyen <nguyentienhaininh@gmail.com>
2021-08-29 02:26:24 +0700

    update menu

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -161,7 +161,7 @@ export const menu: CoreMenu[] = [
             translate: 'MENU.EQUIPMENT_MANAGEMENT.HSM',
             type: 'item',
             icon: 'tool',
-            url: '/apps/equipment-management/hsm'
+            url: '/apps/equipment-management/new-hsm'
           },
           {
             id: "token-management",
@@ -169,7 +169,7 @@ export const menu: CoreMenu[] = [
             translate: 'MENU.EQUIPMENT_MANAGEMENT.TOKEN',
             type: 'item',
             icon: 'tablet',
-            url: '/apps/equipment-management/token'
+            url: '/apps/equipment-management/new-token'
           }
         ]
       },
