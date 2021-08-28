a50ab6f57c86df350a730c1ce0238a45caeae0a9
Ninh Nguyen <nguyentienhaininh@gmail.com>
2021-08-29 02:25:52 +0700

    update token list

--- src/app/main/apps/equipment-management/token-management/token-management.module.ts
+++ src/app/main/apps/equipment-management/token-management/token-management.module.ts
@@ -2,14 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TokenManagementComponent } from './token-management.component';
 import { CoreCommonModule } from '@core/common.module';
+import { NgbCollapseModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { RouterModule, Routes } from '@angular/router';
+import { NewTokenModule } from '../new-token/new-token.module';
 
-
+const routes: Routes = [
+  {
+    path: 'new-token',
+    component: NewTokenModule
+  }
+]
 
 @NgModule({
   declarations: [TokenManagementComponent],
   imports: [
     CommonModule,
-    CoreCommonModule
+    CoreCommonModule,
+    NgbModule,
+    NgbCollapseModule,
+    NgSelectModule,
+    RouterModule.forChild(routes)
   ],
   exports: [TokenManagementComponent]
 })
