8e282f9e1a088efd72759b649da7d7d9714437e8
Ninh Nguyen <nguyentienhaininh@gmail.com>
2021-08-29 02:24:18 +0700

    update hsm list

--- src/app/main/apps/equipment-management/hsm-management/hsm-management.module.ts
+++ src/app/main/apps/equipment-management/hsm-management/hsm-management.module.ts
@@ -2,14 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HsmManagementComponent } from './hsm-management.component';
 import { CoreCommonModule } from '@core/common.module';
+import { Routes, RouterModule } from '@angular/router';
+import { NewHsmComponent } from '../new-hsm/new-hsm.component';
+import { NgbCollapseModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NewTokenModule } from '../new-token/new-token.module';
 
-
+const routes: Routes = [
+  {
+    path: 'new-hsm',
+    component: NewHsmComponent,
+  },
+  {
+    path: 'new-token',
+    component: NewTokenModule
+  }
+]
 
 @NgModule({
   declarations: [HsmManagementComponent],
   imports: [
     CommonModule,
-    CoreCommonModule
+    CoreCommonModule,
+    RouterModule.forChild(routes),
+    NgbCollapseModule,
+    NgSelectModule,
+    NgbModule
   ],
   exports: [HsmManagementComponent]
 })
