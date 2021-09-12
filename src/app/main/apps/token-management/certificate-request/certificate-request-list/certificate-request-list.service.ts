48a662ff87c8e28547d88c0dea9ab38ae150294b
Ninh Nguyen <nguyentienhaininh@gmail.com>
2021-09-12 18:22:44 +0700

    tim kiem yeu cau chung thuc

--- src/app/main/apps/token-management/certificate-request/certificate-request-list/certificate-request-list.service.ts
+++ src/app/main/apps/token-management/certificate-request/certificate-request-list/certificate-request-list.service.ts
@@ -1,14 +1,67 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'environments/environment';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import * as forge from 'node-forge';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CertificateRequestListService {
-  constructor(private http: HttpClient) { }
+export class CertificateRequestListService implements Resolve<any> {
+  public rows: any;
+  public onUserListChanged: BehaviorSubject<any>;
+  public page=0;
 
-  getData(pageSize, page): any{
+  /**
+   * Constructor
+   *
+   * @param {HttpClient} _httpClient
+   */
+  constructor(private _httpClient: HttpClient) {
+    this.onUserListChanged = new BehaviorSubject({});
+  }
+  
+  /**
+   * Resolver
+   *
+   * @param {ActivatedRouteSnapshot} route
+   * @param {RouterStateSnapshot} state
+   * @returns {Observable<any> | Promise<any> | any}
+   */
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {
+    return new Promise<void>((resolve, reject) => {
+      Promise.all([this.getDataTableRows(this.page)]).then(() => {
+        resolve();
+      }, reject);
+    })
+    
+  }
+
+  getDataTableRows(page): Observable<any> | Promise<any> | any {
+    return new Promise((resolve, reject) => {
+      this._httpClient.get(`${environment.apiUrl}/certificate-request/list?page=${page}&size=10`).subscribe((response: any) => {
+        this.rows = response;
+        console.log(response.data.data);
+        this.onUserListChanged.next(this.rows);
+        resolve(this.rows);
+      }, reject);
+    });
+  }
+
+  readCertificate(cer): any[] {
+    //decode
+    let read: any = forge.pki.certificationRequestFromPem(cer);
+
+    //get attributes
+    read = read.subject.attributes;
+
+    //dich tieng Viet 
+    let res: any[] = JSON.parse( forge.util.decodeUtf8( JSON.stringify(read)));
+    return res;
+  }
+
+  getData(page:number,Item:number): Observable<any[]>{
     const currentUser = JSON.parse(localStorage.getItem('currentUser'));
     const token = currentUser.token;
     const option = {
@@ -16,11 +69,8 @@ export class CertificateRequestListService {
         "Content-Type": "application/json",
         "Authorization": "Bearer " + token
       },
-      params: {
-        "page": page-1,
-        "size": pageSize
-      }
     };
-    return this.http.get(`${environment.apiUrl}/certificate-request/list`, option);
+    return this._httpClient.get<any>(`${environment.apiUrl}/certificate-request/list?page=${page}&size=${Item}`, option);
   }
+
 }
