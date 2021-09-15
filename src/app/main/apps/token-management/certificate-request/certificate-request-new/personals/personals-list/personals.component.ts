6a5a27b6dc65c0e147b67c2500dee24e33f0300f
Ninh Nguyen <nguyentienhaininh@gmail.com>
2021-09-15 23:17:26 +0700

    tao yeu cau chung thuc

--- src/app/main/apps/token-management/certificate-request/certificate-request-new/personals/personals-list/personals.component.ts
+++ src/app/main/apps/token-management/certificate-request/certificate-request-new/personals/personals-list/personals.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import {  FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { CoreSidebarService } from "@core/components/core-sidebar/core-sidebar.service";
 import { CoreConfigService } from "@core/services/config.service";
-import { NgbCalendar, NgbDate, NgbDateParserFormatter, NgbDateStruct, NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { PersonalListService } from "app/main/apps/identity-provider/subscribers/personals/personal-list/personal-list.service";
 import { Subject } from "rxjs";
-import { PersonalsService } from "./personals.service";
+import { takeUntil } from "rxjs/operators";
+import { DateAdapter } from "@angular/material/core";
 
 @Component({
   selector: 'app-personals',
@@ -14,169 +14,99 @@ import { PersonalsService } from "./personals.service";
   encapsulation: ViewEncapsulation.None
 })
 export class PersonalsComponent implements OnInit {
-  public rows;
-  public moreOption = true
-  public page = 0
-  public itemOnPage = 3  
+  minDate: Date;
+  maxDate: Date;
+  public rows: any[] = [];
+  public moreOption = true;
+  public page: number = 0;
   public pageAdvancedEllipses = 1;
-  public totalPages:number
-  public sizePage = [5,10,15,20]
-  public hoveredDate: NgbDate | null = null;
-  public fromDate: NgbDate | null;
-  public toDate: NgbDate | null;
-  public birthDay:NgbDate | null
-  public today = this.calendar.getToday();
-  sexOption:any[] = [
-    "Nam",
-    "Nữ",
-  ]
-  public selectedActive = [];
-  
+  public totalPages: number;
+  public sizePage: number[] = [5, 10, 15, 20];
+  gender: string[] = ["Nam", "Nữ"];
   public searchValue = '';
-
   public item: any;
-  
-  // Decorator
-  // @ViewChild(DatatableComponent) table: DatatableComponent;
-  
-  // Private
-
-  // fake db
-
   private _unsubscribeAll: Subject<any>;
-  formListPersonal: FormGroup;
+  public formListCertificateRequest: FormGroup;
+
 
   /**
-   * Constructor
    *
-   * @param {CoreConfigService} _coreConfigService
-   * @param {UserListService} PersonalListService
-   * @param {CoreSidebarService} _coreSidebarService
-   * @param {NgbModal} modalService
-  */
-
+   * @param _userListService
+   * @param _coreSidebarService
+   * @param modalService
+   * @param fb
+   * @param dateAdapter
+   */
   constructor(
     private _userListService: PersonalListService,
-    private _coreSidebarService: CoreSidebarService,
     private _coreConfigService: CoreConfigService,
-    private modalService: NgbModal,
     private fb: FormBuilder,
-    private calendar: NgbCalendar,
-    public formatter: NgbDateParserFormatter,
-    private modal: NgbModal
-
+    private modal: NgbModal,
+    private dateAdapter: DateAdapter<any>
   ) { 
     this._unsubscribeAll = new Subject();
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(currentYear - 4, 0, 1);
+    this.maxDate = new Date(currentYear + 2, 11, 31);
+    this._coreConfigService.config
+      .pipe(takeUntil(this._unsubscribeAll))
+      .subscribe((config) => {
+        this.dateAdapter.setLocale(config.app.appLanguage);
+      });
   }
 
-  // Public Methods
-  // -----------------------------------------------------------------------------------------------------
-  toggleModal(){
-    console.log("output đóng form")
-    this.modalService.hasOpenModals();
+  ngOnInit(): void {
+    this.formListCertificateRequest = this.fb.group({
+      inputPersonal: [null, Validators.required],
+      fromDate: [null],
+      toDate: [null],
+      sizePage: [this.sizePage[1]],
+      gender: [],
+      birthday: [],
+    });
+    this._userListService
+      .getData(this.page, this.sizePage[1])
+      .subscribe((res: any) => {
+        this.totalPages = res.data.totalPages * 10;
+        this.rows = res.data.data;
+      });
+
   }
+
   toggleSidebar(modalForm, item) {
     this.item = item;
     this.modal.open(modalForm, {size: 'lg'})
   }
-  closeModal(name){
-    this._coreSidebarService.getSidebarRegistry(name).toggleOpen();
+
+  changePage(e) {
+    console.log(typeof e);
+    this.page = e;
+    this._userListService
+      .getData(e - 1, this.formListCertificateRequest.controls.sizePage.value)
+      .subscribe((res: any) => {
+        this.rows = res.data.data;
+      });
   }
 
-  changePage(e){
-    console.log(typeof(e))
-    this.page = e
-    this._userListService.getData(e-1,this.itemOnPage).subscribe((respon:any) =>{
-      this.rows = respon.data.data;
-    })
+  selectItem() {
+    this._userListService
+      .getData(this.page, this.formListCertificateRequest.controls.sizePage.value)
+      .subscribe((res: any) => {
+        this.totalPages = res.data.totalPages * this.formListCertificateRequest.controls.sizePage.value;
+        this.rows = res.data.data;
+      });
   }
 
-  selectItem(e){
-    console.log(e)
-    const item = Number(e)
-    this.itemOnPage = Number(e)
-    this._userListService.getData(this.page,item).subscribe((respon:any) =>{
-      this.totalPages = respon.data.totalPages * 10
-      this.rows = respon.data.data;
-    })
-  }
-
-  updateTable(){
-    this._userListService.getData(this.page,this.itemOnPage).subscribe((respon:any) =>{
-      this.rows = respon.data.data;
-    })
-  }
-
-  onDateSelection(date: NgbDate) {
-    if (!this.fromDate && !this.toDate) {
-      this.fromDate = date;
-    } else if (
-      this.fromDate &&
-      !this.toDate &&
-      date &&
-      date.after(this.fromDate)
-    ) {
-      this.toDate = date;
-    } else {
-      this.toDate = null;
-      this.fromDate = date;
-    }
-  }
-
-  isHovered(date: NgbDate) {
-    return (
-      this.fromDate &&
-      !this.toDate &&
-      this.hoveredDate &&
-      date.after(this.fromDate) &&
-      date.before(this.hoveredDate)
-    );
-  }
-
-  isInside(date: NgbDate) {
-    return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
-  }
-
-  isRange(date: NgbDate) {
-    return (
-      date.equals(this.fromDate) ||
-      (this.toDate && date.equals(this.toDate)) ||
-      this.isInside(date) ||
-      this.isHovered(date)
-    );
-  }
-  onSubmit(){
-    console.log(this.formListPersonal)
-  }
-  // Lifecycle Hooks
-  // -----------------------------------------------------------------------------------------------------
-  /**
-   * On init
-   */
-  ngOnInit(): void {
-    this._userListService.getData(this.page,this.itemOnPage).subscribe((respon:any) =>{
-      this.totalPages = respon.data.totalPages * 10
-      this.rows = respon.data.data;
-    })
-    this.formListPersonal = this.fb.group({
-      inputPersonal: ["", Validators.required],
-      fromDate: [null],
-      toDate: [null],
-      sizePage: [this.sizePage[0]],
-      sexOption:[],
-      birthDay:[]
-    })
-
+  onSubmit() {
+    console.log(this.formListCertificateRequest);
   }
 
   /**
    * On destroy
    */
-   ngOnDestroy(): void {
+  ngOnDestroy(): void {
     // Unsubscribe from all subscriptions
     this._unsubscribeAll.next();
     this._unsubscribeAll.complete();
   }
-  
-
 }
--- src/app/main/apps/token-management/certificate-request/certificate-request-new/personals/personals-list/personals.module.ts
+++ src/app/main/apps/token-management/certificate-request/certificate-request-new/personals/personals-list/personals.module.ts
@@ -1,24 +1,53 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { NgSelectModule } from "@ng-select/ng-select";
+import { CoreCommonModule } from "@core/common.module";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { Ng2FlatpickrModule } from "ng2-flatpickr";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import {
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+  MomentDateAdapter,
+} from "@angular/material-moment-adapter";
+import {
+  DateAdapter,
+  MatNativeDateModule,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+} from "@angular/material/core";
+import { MY_DATE_FORMATS } from "@core/format-data/my-date-formats";
+
 import { PersonalsComponent } from './personals.component';
-import { NgbCollapseModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgSelectModule } from '@ng-select/ng-select';
-import { CoreCommonModule } from '@core/common.module';
 import { SidebarPersonalsComponent } from './sidebar-personals/sidebar-personals.component';
 import { PersonalListService } from 'app/main/apps/identity-provider/subscribers/personals/personal-list/personal-list.service';
 
-
+const materialModules1234 = [
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatNativeDateModule,
+];
 
 @NgModule({
   declarations: [PersonalsComponent, SidebarPersonalsComponent],
   imports: [
     CommonModule,
-    NgbCollapseModule,
-    NgbModule,
     NgSelectModule,
-    CoreCommonModule
+    CoreCommonModule,
+    NgbModule,
+    Ng2FlatpickrModule,
+    ...materialModules1234,
   ],
   exports: [PersonalsComponent],
-  providers: [ PersonalListService],
+  providers: [
+    PersonalListService,
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS },],
 })
 export class PersonalsModule { }
