2f0a5006f2777001cfb3f1a8cdbfa3c9926adc1b
nguyễn việt hưng <32328578+hung-hust-97@users.noreply.github.com>
2021-09-21 01:46:04 +0700

    Delete CertificateRequest.ts

--- src/app/main/models/CertificateRequest.ts
+++
@@ -1,8 +0,0 @@
-import { Keypair } from "./Keypair";
-
-export class CertificateRequest {
-    certificateRequestId:string;
-    certificateRequest:string;
-    certificateRequestCreatedTime:string;
-    keypairInformation:Keypair;
-}
