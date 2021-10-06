18172821f638679c41e73784a2cad8551102ccc8
tien <huutienn46@gmail.com>
2021-10-06 17:43:20 +0700

    loading full

--- src/app/main/apps/equipment-management/new-token/new-token.module.ts
+++ src/app/main/apps/equipment-management/new-token/new-token.module.ts
@@ -5,6 +5,9 @@ import { NewTokenComponent } from './new-token.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ContentHeaderModule } from 'app/layout/components/content-header/content-header.module';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoadingInterceptor } from 'app/main/loading/loading.interceptor';
+import { LoadingService } from 'app/main/loading/loading.service';
 
 const routes: Routes = [
   {
@@ -21,6 +24,14 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     ContentHeaderModule,
     NgSelectModule,
-  ]
+  ],
+  providers: [
+    LoadingService,
+      {
+        provide: HTTP_INTERCEPTORS,
+        useClass: LoadingInterceptor,
+        multi: true
+      }
+  ],
 })
 export class NewTokenModule { }
