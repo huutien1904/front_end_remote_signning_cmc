e4deee7a0dfcf92343f86db16cace62abc039a08
DESKTOP-MQG8672\ASUS <binhkhanh2204@gmail.com>
2022-02-25 15:46:03 +0700

    cập nhật giao diện thuê bao

--- src/app/main/apps/identity-provider/subscribers/subscribers.module.ts
+++ src/app/main/apps/identity-provider/subscribers/subscribers.module.ts
@@ -6,6 +6,7 @@ import { SubscribersRoutingModule } from './subscribers-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { OrganizationsModule } from './organizations/organizations.module';
 import { ServiceModule } from './service/service.module';
+import { SearchSubscribersModule } from '../search-subscribers/search-subscriber.module';
 
 @NgModule({
   declarations: [],
@@ -14,7 +15,7 @@ import { ServiceModule } from './service/service.module';
     PersonalsModule,
     OrganizationsModule,
     SubscribersRoutingModule,
-    ServiceModule
+    ServiceModule,
   ],
   providers: [
   ]
