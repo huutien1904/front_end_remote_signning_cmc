9cbe92997b851f1a2d32bfd08da7c75320ef93b6
hunghust <hunga1k15tv@gmail.com>
2022-03-02 14:07:26 +0700

    change enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,9 @@
 export const environment = {
   production: false,
   hmr: false,
-  // apiUrl: 'http://183.91.3.60:8080/csignremote-0.3',
+  apiUrl: 'http://183.91.3.60:8080/csignremote-0.3',
   //apiUrl: 'http://10.0.8.62:8080/csignremote-0.3'
-  apiUrl: 'http://localhost:8080/ssa-0.2'
+  // apiUrl: 'http://localhost:8080/ssa-0.2'
 };
 
 /*
